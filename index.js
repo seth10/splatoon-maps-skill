6310121404be86814f15f35a88277b324443344e
sethten <sethten@users.noreply.github.com>
2016-11-26 13:15:04 -0500

    Working
    Bypassed handler functionality, will have to put this back in to handle
    different type of requests, but for now something is functional

--- index.js
+++ index.js
@@ -7,19 +7,11 @@ var request = require('request');
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
     alexa.appId = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
-};
-
-var handlers = {
-    'GetCurrentMapsIntent': function () {
-        request({
-            url: 'http://splatoon.ink/schedule.json',
-            json: true
-        }, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
-                this.emit(':tell', `The current maps are ${body.schedule[0].regular.maps[0].name.en} and ${body.schedule[0].regular.maps[1].name.en} for Turf Wars, and ${body.schedule[0].ranked.maps[0].name.en} and ${body.schedule[0].ranked.maps[1].name.en} for ${body.schedule[0].ranked.rules.en}.`);
-            }
-        });
-    }
+    
+    request({
+        url: 'http://splatoon.ink/schedule.json',
+        json: true
+    }, function (error, response, body) {
+        alexa.emit(':tell', `The current maps are ${body.schedule[0].regular.maps[0].name.en} and ${body.schedule[0].regular.maps[1].name.en} for Turf Wars, and ${body.schedule[0].ranked.maps[0].name.en} and ${body.schedule[0].ranked.maps[1].name.en} for ${body.schedule[0].ranked.rules.en}.`);
+    });
 };
