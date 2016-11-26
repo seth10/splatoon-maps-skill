b4a6370ed2e2d9462ebc22bd14d2faebf9adff1f
sethten <sethten@users.noreply.github.com>
2016-11-26 17:17:23 -0500

    More Logical Divide of Current and Next
    The wording for the "current" and "next" intents were very similar, so
    I added logic to combine them and only change the differing parts.
    The only thing that didn't quite match up was "The current [mode] maps
    are [map] and [map]." and "The next ranked maps are [mode] on [map] and
    [map]."

--- index.js
+++ index.js
@@ -12,36 +12,49 @@ exports.handler = function(event, context, callback) {
         url: 'http://splatoon.ink/schedule.json',
         json: true
     }, function (error, response, body) {
+        
+        var schedule, timeAdjective;
         switch (event.request.intent.name) {
             case 'GetCurrentMapsIntent':
-                alexa.emit(':tell', `The current maps are ${body.schedule[0].regular.maps[0].name.en} and ${body.schedule[0].regular.maps[1].name.en} for Turf Wars, and ${body.schedule[0].ranked.maps[0].name.en} and ${body.schedule[0].ranked.maps[1].name.en} for ${body.schedule[0].ranked.rules.en}.`);
-                break;
             case 'GetCurrentRankedMapsIntent':
-                alexa.emit(':tell', `The current ${body.schedule[0].ranked.rules.en} maps are ${body.schedule[0].ranked.maps[0].name.en} and ${body.schedule[0].ranked.maps[1].name.en}.`);
-                break;
             case 'GetCurrentTurfWarMapsIntent':
-                alexa.emit(':tell', `The current Turf Wars maps are ${body.schedule[0].regular.maps[0].name.en} and ${body.schedule[0].regular.maps[1].name.en}.`);
-                break;
             case 'GetCurrentRankedModeIntent':
-                alexa.emit(':tell', `The current ranked mode is ${body.schedule[0].ranked.rules.en}.`);
+                schedule = body.schedule[0];
+                timeAdjective = 'current';
                 break;
             
             case 'GetUpcomingMapsIntent':
-                alexa.emit(':tell', `The next maps are ${body.schedule[1].regular.maps[0].name.en} and ${body.schedule[1].regular.maps[1].name.en} for Turf Wars, and ${body.schedule[1].ranked.maps[0].name.en} and ${body.schedule[1].ranked.maps[1].name.en} for ${body.schedule[1].ranked.rules.en}.`);
-                break;
             case 'GetUpcomingRankedMapsIntent':
-                alexa.emit(':tell', `The next ranked maps are ${body.schedule[1].ranked.rules.en} on ${body.schedule[1].ranked.maps[0].name.en} and ${body.schedule[1].ranked.maps[1].name.en}.`);
-                break;
             case 'GetUpcomingTurfWarMapsIntent':
-                alexa.emit(':tell', `The next Turf Wars maps ${body.schedule[1].regular.maps[0].name.en} and ${body.schedule[1].regular.maps[1].name.en}.`);
-                break;
             case 'GetUpcomingRankedModeIntent':
-                alexa.emit(':tell', `The next ranked mode is ${body.schedule[1].ranked.rules.en}.`);
+                schedule = body.schedule[1];
+                timeAdjective = 'next';
+                break;
+        }
+        
+        switch (event.request.intent.name) {
+            case 'GetCurrentMapsIntent':
+            case 'GetUpcomingMapsIntent':
+                alexa.emit(':tell', `The ${timeAdjective} maps are ${schedule.regular.maps[0].name.en} and ${schedule.regular.maps[1].name.en} for Turf Wars, and ${schedule.ranked.maps[0].name.en} and ${schedule.ranked.maps[1].name.en} for ${schedule.ranked.rules.en}.`);
+                break;
+            case 'GetCurrentRankedMapsIntent':
+            case 'GetUpcomingRankedMapsIntent':
+                //alexa.emit(':tell', `The ${timeAdjective} ${schedule.ranked.rules.en} maps are ${schedule.ranked.maps[0].name.en} and ${schedule.ranked.maps[1].name.en}.`);
+                alexa.emit(':tell', `The ${timeAdjective} ranked maps are ${schedule.ranked.rules.en} on ${schedule.ranked.maps[0].name.en} and ${schedule.ranked.maps[1].name.en}.`);
+                break;
+            case 'GetCurrentTurfWarMapsIntent':
+            case 'GetUpcomingTurfWarMapsIntent':
+                alexa.emit(':tell', `The ${timeAdjective} Turf Wars maps are ${schedule.regular.maps[0].name.en} and ${schedule.regular.maps[1].name.en}.`);
+                break;
+            case 'GetCurrentRankedModeIntent':
+            case 'GetUpcomingRankedModeIntent':
+                alexa.emit(':tell', `The ${timeAdjective} ranked mode is ${schedule.ranked.rules.en}.`);
                 break;
             
             case 'GetRotationTimeIntent':
-                alexa.emit(':tell', `The next map ends at ${new Date(body.schedule[0].endTime)}, in ${new Date() - new Date(body.schedule[0].endTime)}.`); // need to format
+                alexa.emit(':tell', `The next map ends at ${new Date(schedule.endTime)}, in ${new Date() - new Date(schedule.endTime)}.`); // need to format
                 break;
         }
-    });
-};
+        
+    }); // close request
+}; // close exports.handler
